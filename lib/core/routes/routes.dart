4576ff700129bac553bbe0d284a315f543f0920e
Nahid-web <nahidamin266@gmail.com>
2025-03-21 03:12:41 +0600

    add loader and snackbar in core folder

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,14 +1,16 @@
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
+import 'package:blog_app/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class RouteNames {
   static const String signUp = '/signup';
   static const String login = '/login';
+  static const String home = '/home';
 }
 
-// GoRouter Configuration
+// GoRouter configuration
 final GoRouter router = GoRouter(
   initialLocation: RouteNames.signUp,
   routes: [
@@ -22,8 +24,13 @@ final GoRouter router = GoRouter(
       name: 'login',
       builder: (context, state) => const LoginPage(),
     ),
+    GoRoute(
+      path: RouteNames.home,
+      name: 'home',
+      builder: (context, state) => const HomePage(),
+    ),
   ],
   errorPageBuilder:
       (context, state) =>
-          MaterialPage(child: const Center(child: Text("error"))),
+          const MaterialPage(child: Center(child: Text("error"))),
 );
