a4c7092bc21064915e51bab180b4b1c2c9b5aca9
Nahid-web <nahidamin266@gmail.com>
2025-03-23 16:25:42 +0600

    feat: add blog page and remove home page

---
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BlogPage extends StatelessWidget {
+  const BlogPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Blog Page')),
+      body: const Center(child: Text('You are Logged in')),
+    );
+  }
+}
