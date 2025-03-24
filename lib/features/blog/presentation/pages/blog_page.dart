4fdafa50ae2ac18fb44872895f02355e44e2c7f3
Nahid-web <nahidamin266@gmail.com>
2025-03-25 01:05:29 +0600

    feat: refactor BlogPage to use StatefulWidget and implement blog fetching with Bloc

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,13 +1,71 @@
+import 'package:blog_app/core/common/widgets/loader.dart';
+import 'package:blog_app/core/routes/routes.dart';
+import 'package:blog_app/core/theme/app_palette.dart';
+import 'package:blog_app/core/utils/show_snackbar.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    context.read<BlogBloc>().add(BlogFetchAllBlogs());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Blog Page')),
-      body: const Center(child: Text('You are Logged in')),
+      appBar: AppBar(
+        title: const Text('Blog Page'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.pushReplacement(RouteNames.blogAddNewBlog);
+            },
+            icon: const Icon(Icons.add_circle),
+          ),
+        ],
+      ),
+
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            showSnackBar(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Loader();
+          }
+
+          if (state is BlogDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return BlogCard(
+                  blog: blog,
+                  color:
+                      index % 2 == 0
+                          ? AppPalette.gradient1
+                          : AppPalette.gradient2,
+                );
+              },
+            );
+          }
+          return const SizedBox();
+        },
+      ),
     );
   }
 }
